f7af82d1c98e947659c0eafeb439316535307d01
saranga weerasekara <129357753+it22215406@users.noreply.github.com>
2025-05-05 21:52:56 +0530

    Update UpdateLearningProgress.js

--- frontend/src/Pages/LearningProgressManagement/UpdateLearningProgress.js
+++ frontend/src/Pages/LearningProgressManagement/UpdateLearningProgress.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';// Import React and hooks
 import { useParams } from 'react-router-dom'; // Import useParams to get route parameters
 import NavBar from '../../Components/NavBar/NavBar';  // Import NavBar component
 
-function UpdateLearningProgress() {  // Get the 'id' parameter from the URL
-  const { id } = useParams();
-  const [formData, setFormData] = useState({
+function UpdateLearningProgress() {  
+  const { id } = useParams();  // Get the 'id' parameter from the URL
+  const [formData, setFormData] = useState({// State to store form data
     title: '',
     description: '',
     date: '',
